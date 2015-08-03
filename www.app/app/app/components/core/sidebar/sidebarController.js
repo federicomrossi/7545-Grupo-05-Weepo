d173ff2671220dfaad0ac492fcad40329924ab87
Federico Rossi <federicorossi@localhost.localdomain>
2015-08-03 15:24:52 -0300

    Documentación del sidebar, para agregar items y subitems

--- www.app/app/app/components/core/sidebar/sidebarController.js
+++ www.app/app/app/components/core/sidebar/sidebarController.js
@@ -12,10 +12,23 @@ angular.module('app')
   	
 
 	$scope.menuItems = [
-        /*{ 'name': 'Dashboard', 'link': '#', 'icon': 'fa-dashboard', 'subitems': [
-        	{ 'name': 'Subitem1',	'link': '#/main/index',	'icon': 'fa-circle-o'},
-        	{ 'name': 'Subitem2',	'link': '#/main/about',	'icon': 'fa-circle-o'}
-        ]},*/
+
+        // Forma de uso:
+        //
+        // { 'name': 'NOMBRE_ITEM', 'link': 'LINK_A_REDIRECCIONAR', 'icon': 'ICONO_FONT_AWASOME', 'subitems': [
+        //    { 'name': 'NOMBRE_SUBITEM', 'link': 'LINK_A_REDIRECCIONAR', 'icon': 'ICONO_FONT_AWASOME'},
+        // ]},
+        //
+        // NOTA: Si tiene subitems, el link del item debe contener '#' en lugar del link. Esto es para que aparezca
+        // el ícono que permite expandir o contraer el submenú.
+        //
+        // Ejemplo:
+        //
+        // { 'name': 'Item', 'link': '#', 'icon': 'fa-dashboard', 'subitems': [
+        // { 'name': 'Subitem1',	'link': '#/main/index',	'icon': 'fa-circle-o'},
+        // { 'name': 'Subitem2',	'link': '#/main/about',	'icon': 'fa-circle-o'}
+        // ]},
+
         { 'name': 'Main', 'link': '#/main', 'icon': 'fa-dashboard', 'subitems': [
         ]},
         { 'name': 'About', 'link': '#/about', 'icon': 'fa-info', 'subitems': [
