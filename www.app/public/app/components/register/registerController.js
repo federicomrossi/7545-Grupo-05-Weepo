d5b7de88f42c76dce6f03c330ea2170b0b8f3bc5
rojasa <rojasagustin90@gmail.com>
2015-08-10 20:03:32 -0300

    pantalla de login

---
+++ www.app/public/app/components/register/registerController.js
@@ -0,0 +1,14 @@
+ 'use strict';
+
+/**
+ * @ngdoc function
+ * @name app.controller:LoginCtrl
+ * @description
+ * # LoginCtrl
+ * Controller of the app
+ */
+angular.module('app')
+  .controller('RegisterCtrl', function ($scope, $http, $location, authorizeService) {
+
+ 
+  });
