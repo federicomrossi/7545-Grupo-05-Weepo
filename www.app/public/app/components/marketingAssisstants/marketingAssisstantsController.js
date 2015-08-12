0c87dc6775a66d6ba3b188caed5dcec8179a4ee0
GFibrizo <zebas.graffe@gmail.com>
2015-08-12 01:36:27 -0300

    Agregados controlador y vista de la pantalla de asesores de marketing

---
+++ www.app/public/app/components/marketingAssisstants/marketingAssisstantsController.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name app.controller:MarketingAssisstantsCtrl
+ * @description
+ * # MarketingAssisstantsCtrl
+ * Controller of the app's Marketing Assisstants page
+ */
+angular.module('app')
+  .controller('MarketingAssisstantsCtrl', function ($scope) {
+  	
+  });
