3f3bf45eb25c0727e61a202d35ba34d0f68deaf7
GFibrizo <zebas.graffe@gmail.com>
2015-08-17 22:18:51 -0300

    Agregadas librerias para subir imagen y controlador y vista de nueva pantalla de editar campaña

---
+++ www.app/public/app/components/newCampaign/newCampaignController.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name app.controller:newCampaignCtrl
+ * @description
+ * # newCampaignCtrl
+ * Controller of the app's New Campaign page
+ */
+angular.module('app')
+  .controller('newCampaignCtrl', function ($scope) {
+  	
+  });
