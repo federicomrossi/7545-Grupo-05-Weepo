abbadb625a1ddbb2b97409b3a1360656206a78e7
rojasa <rojasagustin90@gmail.com>
2015-08-19 00:20:27 -0300

    scrolleando

--- www/js/custom.js
+++ www/js/custom.js
@@ -401,20 +401,20 @@ jQuery(function($){
 	//MENU SCROLLING WITH ACTIVE ITEM SELECTED
 
 	// Cache selectors
-	var lastId,
-	topMenu = $("#top-menu"),
-	topMenuHeight = topMenu.outerHeight()+13,
+	var lastId;
+	var topMenu = $("#top-menu");
+	var topMenuHeight = topMenu.outerHeight()+13;
 	// All list items
-	menuItems = topMenu.find("a"),
+	//var menuItems = topMenu.find("a");
 	// Anchors corresponding to menu items
-	scrollItems = menuItems.map(function(){
+	/*var scrollItems = menuItems.map(function(){
 	  var item = $($(this).attr("href"));
 	  if (item.length) { return item; }
-	});
+	});*/
 
 	// Bind click handler to menu items
 	// so we can get a fancy scroll animation
-	menuItems.click(function(e){
+	$(".menuItems").click(function(e){
 	  var href = $(this).attr("href"),
 	      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
 	  $('html, body').stop().animate({ 
